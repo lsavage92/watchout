1ccfb2493821ea3e2e3b1d9d6a57e097ea156fbc
Luke Savage <luke.savage92@gmail.com>
2015-08-18 17:42:27 -0500

    Added scoring and sweepers

--- watchout.js
+++ watchout.js
@@ -1,4 +1,5 @@
 // start slingin' some d3 here.
+var collisions = 0, score = 0, highScore = 0;
 
 var svgArea = d3.select("body").append("svg")
 .attr("width", 900)
@@ -15,20 +16,22 @@ var borderPath = svgArea.append("rect")
 
 
 var enemyRadii = [];
+
 //create array with all attributes of enemy
-for(var i = 0; i < 25; i++){
+for(var i = 0; i < 36; i++){ // only 24 because of < instead of <=
   var x = Math.floor(Math.random() * 800) + 10;
   var y = Math.floor(Math.random() * 500) + 10;
   var r = 10;
+
   enemyRadii.push([r, x, y]);
 }
 
 var randomX = function(){
-  return Math.floor(Math.random() * 800) + 10;
+  return Math.floor(Math.random() * 880) + 10;
 }
 
 var randomY = function(){
-  return Math.floor(Math.random() * 500) + 10;
+  return Math.floor(Math.random() * 580) + 10;
 }
 
 var randomDelay = function(){
@@ -49,6 +52,42 @@ var player = svgArea.append("circle")
   .style("fill", "blue")
   .call(drag);
 
+var enemies = svgArea.selectAll("circle")
+    .data(enemyRadii)
+    .enter()
+    .append("circle");
+
+var sweepers = svgArea.selectAll("rect")
+  .data([[450, 0],[450, 600], [0, 0]])
+  .enter()
+  .append("rect");
+
+var sweepCount = 0;
+var sweeperAttr = sweepers
+  .attr("width", 30)
+  .attr("height", 30)
+  .attr("x", function(d){
+    if (sweepCount === 0){
+      sweepCount++;
+      return 0;
+    }
+    else{
+      sweepCount = 0;
+      return 870;
+    }
+  })
+  .attr("y", function(d){
+    if (sweepCount === 0){
+      sweepCount++;
+      return 0;
+    }
+    else{
+      sweepCount = 0;
+      return 570;
+    }
+  })
+  .style("fill", "orange");
+
 function dragmove(d) {
   var x = d3.event.x;
   var y = d3.event.y;
@@ -57,13 +96,50 @@ function dragmove(d) {
   .attr('cy', y);//.attr("transform", "translate(" + x + "," + y + ")");
 }
 
+var prevCollision = false;
+
+// Collision detection - enemies
+function checkEnemyCollision(){
+  var collision = false;
+
+  enemies.each(function(d, i){
+    var xDiff = Math.abs(d[1] - player.attr("cx"));
+    var yDiff = Math.abs(d[2] - player.attr("cy"));
+    var radiiSum = JSON.parse(d[0]) + JSON.parse(player.attr("r"));
+    var distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+
+    if( distance < radiiSum){
+      collision = true;
+    }
+  });
+
+  if(collision){
+    score = 0;
+    if(collision != prevCollision){
+      collisions += 1;
+    }
+  }
+  prevCollision = collision;
+}
+d3.timer(checkEnemyCollision);
+
+
+//   // var radiusSum = enemies.attr("r") + player.attr("r");
+//   // var xDiff =
+//   // var yDiff =
+  // var pX = player.attr("cx");
+  // var pY = player.attr("cy");
+  // var eX = [];
+  // var eY = [];
+
+  // enemies.each( function(){
+  //   eX.push(enemies.attr("cx"));
+  // });
+
 
 //Loops the enemies movements to random locations with a random delay
 function looper(){
-  var enemies = svgArea.selectAll("circle")
-    .data(enemyRadii)
-    .enter()
-    .append("circle")
+    enemies
     .attr("r", function(d){ return d[0]; }) //returns radii in array
     .attr("cx", function(d){ return d[1]; })
     .attr("cy", function(d){ return d[2]; })
@@ -79,10 +155,56 @@ function looper(){
         .attr("cx", randomX)
         .attr("cy", randomY)
         .each("end",repeat);
-    }
+      if (sweepers.attr('x') > 869 && sweepers.attr('y')< 20){//570, width 870
+        sweepers.transition()
+          .attr("y", 570);
+      }
+      if(sweepers.attr('x') < 20 && sweepers.attr('y') < 20){
+        sweepers.transition()
+         .attr("x", 870);
+      }
+
+      if(sweepers.attr('x') > 869 && sweepers.attr('y') > 569){
+        sweepers.transition()
+         .attr("x", 0);
+      }
+      if(sweepers.attr('x') < 20 && sweepers.attr('y') > 569)
+        sweepers.transition()
+         .attr("y", 0);
+      }
+
 }
 
 looper();
+
+function scoreTick(){
+  score = score + 1;
+  highScore = Math.max(highScore, score);
+  updateScore();
+}
+setInterval(scoreTick, 100);
+
+function updateScore(){
+  d3.select('.scoreboard .current span').text(score);
+  d3.select('.scoreboard .high span').text(highScore);
+  d3.select('.scoreboard .collisions span').text(collisions);
+}
+  // force
+  //     .nodes(graph.nodes)
+  //     .links(graph.links)
+  //     .on("tick", tick)
+  //     .start();
+  // function tick() {
+  //   node.attr("height", function(d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)); })
+  //       .attr("width", function(d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)); });
+
+  //   link.attr("x1", function(d) { return d.source.x; })
+  //       .attr("y1", function(d) { return d.source.y; })
+  //       .attr("x2", function(d) { return d.target.x; })
+  //       .attr("y2", function(d) { return d.target.y; });
+  // }
+
+
 
 //transition for enemies for weaving motion
 //duration()
